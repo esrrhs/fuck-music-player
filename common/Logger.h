59c96fa2d46a4a8b7cabf942c43ec2942502a3f0
esrrhs <esrrhs@esrrhs-PC>
2011-07-24 22:42:24 +0800

    add cegui

---
+++ common/Logger.h
@@ -0,0 +1,30 @@
+#ifndef __LOGGER__
+#define __LOGGER__
+
+#include <boost/asio.hpp>
+#include <boost/logger/logger.hpp>
+#include "globledefine.h"
+
+class Logger
+{
+public:
+	void log(const std::string & file, const std::string & tag, const std::string & str)
+	{
+		if (m_filemap[file] == NULL)
+		{
+			m_filemap[file] = new boost::asio::io_service;
+			m_tagmap[tag] = new services::logger(*m_filemap[file], tag);
+			m_tagmap[tag]->use_file(file);
+		}
+		else if (m_tagmap[tag] == NULL)
+		{
+			m_tagmap[tag] = new services::logger(*m_filemap[file], tag);
+		}
+		m_tagmap[tag]->log(str);
+	}
+private:
+	std::map<std::string, boost::asio::io_service*> m_filemap;
+	std::map<std::string, services::logger*> m_tagmap;
+};
+
+#endif	
