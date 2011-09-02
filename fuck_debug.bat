96f9c9f8a687de5ea45781483bc358d74a45d517
esrrhs <esrrhs@esrrhs-PC>
2011-09-02 23:43:49 +0800

    bat

---
+++ fuck_debug.bat
@@ -0,0 +1,4 @@
+copy lib\Debug\frameadapter.dll fuck\frameadapter.dll
+copy lib\Debug\process.dll fuck\process.dll
+copy lib\Debug\logo.dll fuck\logo.dll
+copy lib\Debug\thread.dll fuck\thread.dll
---
+++ fuck_release.bat
@@ -0,0 +1,5 @@
+copy lib\Release\frameadapter.dll fuck\frameadapter.dll
+copy lib\Release\process.dll fuck\process.dll
+copy lib\Release\logo.dll fuck\logo.dll
+copy lib\Release\thread.dll fuck\thread.dll
+copy bin\Release\FuckMPlayer.exe fuck\FuckMPlayer.exe
