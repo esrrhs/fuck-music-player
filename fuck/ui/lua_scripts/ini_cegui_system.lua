55754d4edc156cf44b5fa7b4124fd5ea75bf9bc2
esrrhs <esrrhs@esrrhs-PC>
2011-10-20 10:03:09 +0800

    yaoshihuan

--- fuck/ui/lua_scripts/ini_cegui_system.lua
+++ fuck/ui/lua_scripts/ini_cegui_system.lua
@@ -18,3 +18,8 @@ root:addChildWindow(wheel);
 guiSystem:setGUISheet(root)
 guiSystem:setDefaultMouseCursor("TaharezLook", "MouseArrow")
 guiSystem:setDefaultTooltip("TaharezLook/Tooltip")
+
+winMgr:getWindow("uiwheel/music_item_0"):setRotation(CEGUI.Vector3(0,0,30));
+winMgr:getWindow("uiwheel/music_item_1"):setRotation(CEGUI.Vector3(0,0,15));
+winMgr:getWindow("uiwheel/music_item_3"):setRotation(CEGUI.Vector3(0,0,-15));
+winMgr:getWindow("uiwheel/music_item_4"):setRotation(CEGUI.Vector3(0,0,-30));
