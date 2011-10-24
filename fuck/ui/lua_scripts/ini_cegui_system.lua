6af57d70c1f71c6a264b1242d781d39575e9e558
esrrhs <esrrhs@esrrhs-PC>
2011-10-24 08:40:04 +0800

    loading

--- fuck/ui/lua_scripts/ini_cegui_system.lua
+++ fuck/ui/lua_scripts/ini_cegui_system.lua
@@ -12,6 +12,55 @@ function click_last(args)
    	winMgr:getWindow("uiwheel/music_item_0"):setText(GetWheelName(2));
 end
 
+function ini_uioading()
+
+    local guiSystem = CEGUI.System:getSingleton()
+    local schemeMgr = CEGUI.SchemeManager:getSingleton()
+    local winMgr = CEGUI.WindowManager:getSingleton()
+    local logger = CEGUI.Logger:getSingleton()  
+
+    logger:logEvent( "ini_uioading begin..." )  
+    local wheel = winMgr:loadWindowLayout("uiloading.layout")
+    
+    local root = guiSystem:getGUISheet()
+    root:addChildWindow(wheel);
+    
+    logger:logEvent( "ini_uiwheel ok" )  
+
+end
+
+function ini_uiwheel()
+
+    local guiSystem = CEGUI.System:getSingleton()
+    local schemeMgr = CEGUI.SchemeManager:getSingleton()
+    local winMgr = CEGUI.WindowManager:getSingleton()
+    local logger = CEGUI.Logger:getSingleton()  
+
+    logger:logEvent( "ini_uiwheel begin..." )  
+    local wheel = winMgr:loadWindowLayout("uiwheel.layout")
+    
+    local root = guiSystem:getGUISheet()
+    root:addChildWindow(wheel);
+    
+    -- 改为layout控制
+    --winMgr:getWindow("uiwheel/music_item_0"):setRotation(CEGUI.Vector3(0,0,30));
+    --winMgr:getWindow("uiwheel/music_item_1"):setRotation(CEGUI.Vector3(0,0,15));
+    --winMgr:getWindow("uiwheel/music_item_3"):setRotation(CEGUI.Vector3(0,0,-15));
+    --winMgr:getWindow("uiwheel/music_item_4"):setRotation(CEGUI.Vector3(0,0,-30));
+
+    winMgr:getWindow("uiwheel/music_item_0"):setText(GetWheelName(0));
+    winMgr:getWindow("uiwheel/music_item_1"):setText(GetWheelName(1));
+    winMgr:getWindow("uiwheel/music_item_2"):setText(GetWheelName(2));
+    winMgr:getWindow("uiwheel/music_item_3"):setText(GetWheelName(3));
+    winMgr:getWindow("uiwheel/music_item_4"):setText(GetWheelName(4));
+
+
+    winMgr:getWindow("uiwheel/next"):subscribeEvent("Clicked", "click_next")
+    winMgr:getWindow("uiwheel/last"):subscribeEvent("Clicked", "click_last")
+
+    logger:logEvent( "ini_uiwheel ok" )  
+end
+
 -----------------------------------------
 -- Script Entry Point
 -----------------------------------------
@@ -26,27 +75,13 @@ schemeMgr:create("TaharezLook.scheme");
 
 local root = winMgr:createWindow("DefaultWindow", "Root")
 
-local wheel = winMgr:loadWindowLayout("uiwheel.layout")
-root:addChildWindow(wheel);
-
 guiSystem:setGUISheet(root)
 guiSystem:setDefaultMouseCursor("TaharezLook", "MouseArrow")
 guiSystem:setDefaultTooltip("TaharezLook/Tooltip")
 
--- 改为layout控制
---winMgr:getWindow("uiwheel/music_item_0"):setRotation(CEGUI.Vector3(0,0,30));
---winMgr:getWindow("uiwheel/music_item_1"):setRotation(CEGUI.Vector3(0,0,15));
---winMgr:getWindow("uiwheel/music_item_3"):setRotation(CEGUI.Vector3(0,0,-15));
---winMgr:getWindow("uiwheel/music_item_4"):setRotation(CEGUI.Vector3(0,0,-30));
+ini_uioading()
+ini_uiwheel()
 
-winMgr:getWindow("uiwheel/music_item_0"):setText(GetWheelName(0));
-winMgr:getWindow("uiwheel/music_item_1"):setText(GetWheelName(1));
-winMgr:getWindow("uiwheel/music_item_2"):setText(GetWheelName(2));
-winMgr:getWindow("uiwheel/music_item_3"):setText(GetWheelName(3));
-winMgr:getWindow("uiwheel/music_item_4"):setText(GetWheelName(4));
-
-
-winMgr:getWindow("uiwheel/next"):subscribeEvent("Clicked", "click_next")
-winMgr:getWindow("uiwheel/last"):subscribeEvent("Clicked", "click_last")
+winMgr:getWindow("uiwheel"):hide()
 
 logger:logEvent( "ini_cegui_system ok" )  
